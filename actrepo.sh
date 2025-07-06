7ed7f32574ea226f7c1bdec2a43a01806c0302a2
iJoseG <josedguerra1107@gmail.com>
2025-07-06 03:03:17 -0500

    Actualizando README y actrepo

--- actrepo.sh
+++ actrepo.sh
@@ -12,18 +12,17 @@ if [[ -z $(git status --porcelain) ]]; then
     echo "No hay cambios para hacer commit. Saliendo..."
     exit 1
 fi
-
-echo "Ahora estás en: $(pwd)"
+git status
+# echo "Ahora estás en: $(pwd)"
 
 git add .
-
 read -rp "Digite mensaje descriptivo para commit: " messss
-
 git commit -m "$messss"
-
 nrama="$(git branch)"
-
 git push origin main
+
+echo " "
+echo " "
 
 git status
 

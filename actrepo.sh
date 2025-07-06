4edc4e6b2fe90c75865280f4a67653da4d0a45c2
iJoseG <josedguerra1107@gmail.com>
2025-07-06 02:29:17 -0500

    Mejorando script para actualizar repo

--- actrepo.sh
+++ actrepo.sh
@@ -6,16 +6,21 @@
 # La ruta del proyecto
 ruta_proyecto="/home/jguerra/Documents/Mscripts"
 
+cd "$ruta_proyecto" || { echo "Error: no se pudo acceder a la ruta $ruta_proyecto"; exit 1; }
+
 cd /home/jguerra/Documents/Mscripts
 echo "Ahora estás en: $(pwd)"
 
-git status
+if [[ -z $(git status porcelain) ]]; then
+    echo "No hay cambios para hacer commit. Saliendo..."
+    exit 0
+fi
 
 git add .
 
 read -rp "Digite mensaje descriptivo para commit: " messss
 
-git commit -m $messss
+git commit -m "$messss"
 
 nrama="$(git branch)"
 

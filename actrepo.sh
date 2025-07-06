3d8ff826f443ec99aa50c15069516b661f79d040
iJoseG <josedguerra1107@gmail.com>
2025-07-06 03:07:48 -0500

    Comentando

--- actrepo.sh
+++ actrepo.sh
@@ -12,14 +12,21 @@ if [[ -z $(git status --porcelain) ]]; then
     echo "No hay cambios por realizar. Saliendo..."
     exit 1
 fi
+
+# Observar el estado del repositorio
 git status
 # echo "Ahora estás en: $(pwd)"
 
+# Agregar los archivos que no estén actualizados
 git add .
+
+# Recivir mensaje para ejecutar commit
 read -rp "Digite mensaje descriptivo para commit: " messss
 git commit -m "$messss"
+
+# Obtenemos branch
 nrama="$(git branch)"
-git push origin main
+git push origin $nrama
 
 echo " "
 echo " "

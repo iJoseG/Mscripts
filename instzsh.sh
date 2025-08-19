d7fa683aff278c081232a4b0b74eedfb01efe2d2
iJoseG <josedavidguerrabocanegra@gmail.com>
2025-08-19 03:38:59 -0500

    asdas

--- instzsh.sh
+++ instzsh.sh
@@ -46,7 +46,7 @@ linea6='ZSH_THEME="powerlevel10k/powerlevel10k"'
 
 
 # Agregar cada línea si no existe
-for linea in "$linea0" "$linea1" "$linea2" "$linea3" "$linea4" "$linea5" "$linea6"; do
+for linea in "$linea0" "$linea1" "$linea2" "$linea3" "$linea4" "$linea5"; do
     if ! grep -Fxq "$linea" ~/.zshrc; then
         echo "$linea" >> ~/.zshrc
         echo "Agregado: $linea"

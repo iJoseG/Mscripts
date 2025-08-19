55cbeded43a40a7249f0de6cef0c470edc663652
iJoseG <josedavidguerrabocanegra@gmail.com>
2025-08-19 11:34:32 -0500

    Update instzsh.sh

--- instzsh.sh
+++ instzsh.sh
@@ -58,6 +58,7 @@ zsh
 
 sed -i 's|^ZSH_THEME=.*|ZSH_THEME="powerlevel10k/powerlevel10k"|' ~/.zshrc
 
+source ~/.zshrc
 git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k 
 
 #zsh $(source ~/.zshrc)

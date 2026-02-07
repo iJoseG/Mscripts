00771ceba86bc0d8a2709b89a1064df731ef5d51
iJoseG <josedavidguerrabocanegra@gmail.com>
2026-02-07 13:05:16 -0500

    Scripts nuevos para instalar mi configuracion de nvim y alacritty

---
+++ m_configs/conf_alacritty.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+mkdir -p ~/.config/alacritty
+
+cat << 'EOF' > ~/.config/alacritty/alacritty.toml
+# ===== CONFIGS ALACRITTY =====
+
+[general]
+import = ["~/.config/alacritty/themes/noctalia.toml"]
+
+[font]
+size = 12
+
+[font.normal]
+family = "JetBrainsMono Nerd Font"
+style = "Regular"
+
+[cursor]
+style = { shape = "Beam", blinking = "On" }
+thickness = 0.15
+#style = "Beam"      # Block | Underline | Beam
+#blinking = "On"         # On | Off | Always | Never
+
+[window]
+opacity = 0.8
+decorations = "None"
+
+[window.padding]
+x = 10
+y = 8
+EOF
